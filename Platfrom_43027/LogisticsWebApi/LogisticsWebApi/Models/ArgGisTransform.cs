5ed79f2c866db6c58ff41522f1a5325b8ce663ad
wangze@gsafety.com <wangze@gsafety.com>
2019-12-30 10:17:45 -0400

    修改系统功能

---
+++ Platfrom_43027/LogisticsWebApi/LogisticsWebApi/Models/ArgGisTransform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogisticsWebApi.Models
+{
+    public class ArgGisTransformRoot
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<GeometriesItem> geometries { get; set; }
+
+    }
+
+    //如果好用，请收藏地址，帮忙分享。
+    public class GeometriesItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public double x { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double y { get; set; }
+    }
+
+}
